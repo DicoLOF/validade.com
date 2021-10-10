c004ac898b599ff6a90d473a7e3b893d3d85e3f3
DicoLOF <osvaldo_filho@live.com>
2021-10-10 15:10:00 -0300

    index

--- index.php
+++ index.php
@@ -9,14 +9,14 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!--===============================================================================================-->    
-    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="assets/vendor/bootstrap/css/bootstrap.min.css" />
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css" />
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css" />
+    <link rel="stylesheet" type="text/css" href="assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css" />
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="css/util.css" />
-    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/util.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css" />
     <!--===============================================================================================-->
   </head>
   <body>
@@ -52,7 +52,7 @@
     </div>
     <div id="dropDownSelect1"></div>
     <!--===============================================================================================-->
-    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <script src="assets/vendor/jquery/jquery-3.2.1.min.js"></script>
     <!--===============================================================================================-->    
     <script src="js/main.js"></script>
   </body>
