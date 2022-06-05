00bb0cd0952e95a7bd12acdf2cec165d903844b4
DicoLOF <osvaldo_filho@live.com>
2022-06-05 12:46:56 -0300

    upload

--- php/index.php
+++ php/index.php
@@ -1,15 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">    
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> <?php echo $_GET['pg'] ?> </title>
 </head>
 
 <body>
-    <h3>CARTAZ DIGITAL</h3>
+    <h3>VALIDADE DOS PRODUTOS</h3>
     <a href="main">HOME</a><br>
     <a href="teste">TESTE</a>
     <?php include_once 'pages/url.php'; ?>
